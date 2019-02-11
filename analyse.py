e99285989feff2414130bdcc7e4625b3e032f5f6
Saku Pietilä <spietila@iki.fi>
2019-02-11 13:36:06 +0200

    IP checks added for private addresses, IPv4 and 6 checks

--- analyse.py
+++ analyse.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-# Author: Arclighz
+# Author: Saku Pietila
+# 11.02.2019
 
 from email.parser import HeaderParser
 import sys
@@ -8,6 +9,8 @@ from ipwhois import IPWhois
 from pprint import pprint
 import datetime
 from termcolor import colored
+import socket
+import struct
 
 # Styles for terminal just for the lulz
 class style:
@@ -108,13 +111,94 @@ def parse_received():
         else:
                 pass
     findIP = re.findall(ipPattern,received_headers[-1])
-    #from IPython import embed; embed()
     sender_ip_address = list(set(findIP))
-    print colored(style.BOLD + '\n---------- Sender IP based on Received headers ---------' + style.END, 'blue')
-    print('Sender address: %s' % " ".join(str(x) for x in sender_ip_address))
-    print('Found in : ' + received_headers[-1])
-    return sender_ip_address[0]
+    
+    for ip in sender_ip_address:
+        if is_valid_ipv4_address(ip) == True:
+            if is_private_ip(ip) == True:
+                ip = ""
+            else:
+                #from IPython import embed; embed()
+                print colored(style.BOLD + '\n---------- Sender IP based on Received headers ---------' + style.END, 'blue')
+                print('Sender address: %s' % ip)
+                print('Found in : ' + received_headers[-1])
+                print ip
+                return ip
+        else:
+            return False
 
+
+def is_valid_ipv4_address(address):
+    """Check if the IP is IPv4.
+    @param address: IP address to verify.
+    @return: boolean representing whether the IP belongs or not to
+             a IPv4.
+    """
+    try:
+        socket.inet_pton(socket.AF_INET, address)
+    except AttributeError:  # no inet_pton here, sorry
+        try:
+            socket.inet_aton(address)
+        except socket.error:
+            return False
+        return address.count('.') == 3
+    except socket.error:  # not a valid address
+        return False
+
+    return True
+
+def is_valid_ipv6_address(address):
+    """Check if the IP is IPv6.
+    @param address: IP address to verify.
+    @return: boolean representing whether the IP belongs or not to
+             a IPv6.
+    """
+    try:
+        socket.inet_pton(socket.AF_INET6, address)
+    except socket.error:  # not a valid address
+        return False
+    return True    
+
+def is_private_ip(ip):
+    """Check if the IP belongs to private network blocks.
+    @param ip: IP address to verify.
+    @return: boolean representing whether the IP belongs or not to
+             a private network block.
+    """
+    networks = [
+        "0.0.0.0/8",
+        "10.0.0.0/8",
+        "100.64.0.0/10",
+        "127.0.0.0/8",
+        "169.254.0.0/16",
+        "172.16.0.0/12",
+        "192.0.0.0/24",
+        "192.0.2.0/24",
+        "192.88.99.0/24",
+        "192.168.0.0/16",
+        "198.18.0.0/15",
+        "198.51.100.0/24",
+        "203.0.113.0/24",
+        "240.0.0.0/4",
+        "255.255.255.255/32",
+        "224.0.0.0/4",
+    ]
+
+    for network in networks:
+        try:
+            ipaddr = struct.unpack(">I", socket.inet_aton(ip))[0]
+
+            netaddr, bits = network.split("/")
+
+            network_low = struct.unpack(">I", socket.inet_aton(netaddr))[0]
+            network_high = network_low | 1 << (32 - int(bits)) - 1
+
+            if ipaddr <= network_high and ipaddr >= network_low:
+                return True
+        except:
+            continue
+
+    return False
 
 def main():
     
@@ -122,18 +206,21 @@ def main():
                     + str(datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')) \
                     + '---------' + style.END, 'blue')
     find_spf_header()
-    #from IPython import embed; embed()
 
     if find_spf_header_ip() != None:
         print colored(style.BOLD + '---------- Checking details for SPF IP ---------' + style.END, 'blue')
         ip_whois(find_spf_header_ip())
     else:
-        pass
+        print colored(style.BOLD + '---------- No SPF IP found ---------' + style.END, 'blue')
+        parse_received()
+        print colored(style.BOLD + '---------- Checking details for Received IP ---------' + style.END, 'blue')
+        #from IPython import embed; embed()
+        if parse_received() != False:
+            if is_private_ip(parse_received()) != False:
+                ip_whois(parse_received())
+            else:
+                print colored(style.BOLD + 'IP adderess of the sender in from private subnet, please check network documentation' + style.END, 'blue')
     
-    if find_spf_header_ip() != None:
-        ip_whois(parse_received())
-    else:
-        pass
     #parse_received()
 
     # Python debugger
